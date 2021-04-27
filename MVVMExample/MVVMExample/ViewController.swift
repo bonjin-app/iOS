67cfcec168324da9d160b82499a040e1ecec7619
bonjin-all <bonjin.app@gmail.com>
2021-04-27 09:34:32 +0900

    Update TableView Code

--- MVVMExample/MVVMExample/ViewController.swift
+++ MVVMExample/MVVMExample/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableView.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
+}
