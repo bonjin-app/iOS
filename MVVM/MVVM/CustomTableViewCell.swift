6731113aa5565b6d643057d9cdc3ca0c0f5ca653
bonjin-all <bonjin.app@gmail.com>
2021-04-27 09:29:41 +0900

    tableView ViewModel

---
+++ MVVM/MVVM/CustomTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  CustomTableViewCell.swift
+//  MVVM
+//
+//  Created by gigas on 2021/04/27.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    static let cellIdentifier = "CustomTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "CustomTableViewCell", bundle: nil)
+    }
+    
+    public func configure(with viewModel: CellViewModel) {
+        myLabel.text = "\(viewModel.firstName) \(viewModel.lastName)"
+    }
+}
--- MVVM/MVVM/ViewController.swift
+++ MVVM/MVVM/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data = [
+        Person(firstName: "Dan", lastName: "Smith", gender: "male", age: 25, height: 144),
+        Person(firstName: "Betty", lastName: "Smith", gender: "male", age: 25, height: 144),
+        Person(firstName: "John", lastName: "Smith", gender: "male", age: 25, height: 144),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.register(CustomTableViewCell.nib(), forCellReuseIdentifier: CustomTableViewCell.cellIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.cellIdentifier, for: indexPath) as! CustomTableViewCell
+        
+        let model = data[indexPath.row]
+        let viewModel = CellViewModel(firstName: model.firstName, lastName: model.lastName)
+        cell.configure(with: viewModel)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let gender: String
+    let age: Int
+    let height: Double
+}
+
+struct CellViewModel {
+    let firstName: String
+    let lastName: String
+}
