9e39fb216a39e21ea56b964224579478122251de
bonjin-all <bonjin.app@gmail.com>
2021-04-27 09:43:28 +0900

    Add Person Struct

--- MVVMExample/MVVMExample/ViewController.swift
+++ MVVMExample/MVVMExample/ViewController.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
+struct Person {
+    let name: String
+    
+}
+
 class ViewController: UIViewController {
+    
+    private var models = [Person]()
     
     private let tableView: UITableView = {
         let table = UITableView()
@@ -18,20 +25,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureModels()
         view.addSubview(tableView)
         tableView.dataSource = self
         tableView.frame = view.bounds
+    }
+    
+    private func configureModels() {
+        let names = [
+            "Joe", "Dan", "Jeff", "Jenny", "Emily"
+        ]
+        
+        for name in names {
+            models.append(Person(name: name))
+        }
     }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Hello World"
+        cell.textLabel?.text = model.name
         return cell
     }
 }
