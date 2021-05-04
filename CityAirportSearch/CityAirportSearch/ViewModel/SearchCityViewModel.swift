b86af748a35cf1f772e9c23b064c726791f2d7e7
bonjin-all <bonjin.app@gmail.com>
2021-05-04 18:02:34 +0900

    Update ViewModel: SearchCityViewModel Present protocol 추가.

--- CityAirportSearch/CityAirportSearch/ViewModel/SearchCityViewModel.swift
+++ CityAirportSearch/CityAirportSearch/ViewModel/SearchCityViewModel.swift
@@ -6,12 +6,32 @@
 //
 
 import Foundation
+import RxCocoa
 
 protocol SearchCityViewPresentable {
+    typealias Input = (
+        searchText: Driver<String>, ()
+    )
+    typealias Output = ()
     
+    var input: SearchCityViewPresentable.Input { get }
+    var output: SearchCityViewPresentable.Output { get }
 }
 
 class SearchCityViewModel: SearchCityViewPresentable {
     
+    var input: SearchCityViewPresentable.Input
+    var output: SearchCityViewPresentable.Output
     
+    init(input: SearchCityViewPresentable.Input) {
+        self.input = input
+        self.output = SearchCityViewModel.output(input: self.input)
+    }
+}
+
+private extension SearchCityViewModel {
+    static func output(input: SearchCityViewPresentable.Input) -> SearchCityViewPresentable.Output {
+        
+        return ()
+    }
 }
