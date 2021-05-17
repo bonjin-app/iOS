d42b17272a57d2b760b6f2d50e34c0d86d8bd095
bonjin-all <bonjin.app@gmail.com>
2021-05-17 11:53:37 +0900

    Update PDFKit: + Multiple PDF Viewer Function 추가.

--- PDFKitExample/PDFKitExample/ViewController.swift
+++ PDFKitExample/PDFKitExample/ViewController.swift
@@ -17,11 +17,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "http://www.africau.edu/images/default/sample.pdf"
-        if let url = URL(string: urlString),
-           let document = PDFDocument(url: url) {
-            loadPdfView(document: document)
+        let urlStrings = ["http://www.africau.edu/images/default/sample.pdf",
+                         "http://www.africau.edu/images/default/sample.pdf",
+                         "http://www.africau.edu/images/default/sample.pdf"]
+        
+        let documents = urlStrings.compactMap { (urlString) -> PDFDocument? in
+            if let url = URL(string: urlString),
+               let document = PDFDocument(url: url) {
+                return document
+            }
+            return nil
         }
+        loadMultiplePdfView(inputDocuments: documents)
+    }
+    
+    func loadMultiplePdfView(inputDocuments: [PDFDocument]) {
+        let document: PDFDocument = PDFDocument()
+        var pageIndex: Int = .zero
+        for input in inputDocuments {
+            for i in .zero...input.pageCount {
+                if let page: PDFPage = input.page(at: i) {
+                    pageIndex += pageIndex
+                    document.insert(page, at: pageIndex)
+                }
+            }
+        }
+        
+        pdfView.autoScales = true
+        pdfView.displayMode = .singlePageContinuous
+        pdfView.displayDirection = .vertical
+        pdfView.document = document
     }
     
     func loadPdfView(document: PDFDocument) {
@@ -29,8 +54,6 @@ class ViewController: UIViewController {
         pdfView.displayMode = .singlePageContinuous
         pdfView.displayDirection = .vertical
         pdfView.document = document
-        
-        self.pdfDocument = document
     }
 }
 
