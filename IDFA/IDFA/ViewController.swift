f784723b3ac524148fd066ce59068549a59670aa
bonjin-all <bonjin.app@gmail.com>
2021-05-25 13:10:16 +0900

    Init Project

---
+++ IDFA/IDFA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IDFA
+//
+//  Created by gigas on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
