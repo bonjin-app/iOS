9b2f7e280f640a57adaede31991ef5c1998e5420
bonjin-all <bonjin.app@gmail.com>
2021-06-03 15:20:58 +0900

    Init AppsFlyer Project

---
+++ AppsFlyer/AppsFlyer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppsFlyer
+//
+//  Created by gigas on 2021/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
