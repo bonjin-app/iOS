9136ff41ef14cca1a64c71c57440ca739e0565d8
bonjin-all <bonjin.app@gmail.com>
2021-06-09 11:21:55 +0900

    Init FileUpload Project

---
+++ FileUpload/FileUpload/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileUpload
+//
+//  Created by gigas on 2021/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
