37e050452ee460ab464d134eda640d0be5f1ac7b
bonjin-all <bonjin.app@gmail.com>
2021-06-09 17:17:42 +0900

    Update Storyboard: 디자인 수정.

--- FileUpload/FileUpload/ViewController.swift
+++ FileUpload/FileUpload/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var uploadButton: UIButton!
