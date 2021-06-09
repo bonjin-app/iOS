10399c37913e0c3e516e695d9821c64cf8f4c88e
bonjin-all <bonjin.app@gmail.com>
2021-06-09 15:45:41 +0900

    Update Sync

---
+++ FileUpload/FileUpload/Providers/FileUploadProvider.swift
@@ -0,0 +1,17 @@
+//
+//  FileUploadProvider.swift
+//  FileUpload
+//
+//  Created by gigas on 2021/06/09.
+//
+
+import Foundation
+import Alamofire
+import MobileCoreServices
+
+class FileUploadProvider {
+    
+    static let shared = { FileUploadProvider() }()
+    
+    
+}
