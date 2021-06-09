1b5b8ac3cac6c2f9279e5c7c52edfa8e0978ba2a
bonjin-all <bonjin.app@gmail.com>
2021-06-09 16:23:19 +0900

    Update Provider

--- FileUpload/FileUpload/Providers/FileUploadProvider.swift
+++ FileUpload/FileUpload/Providers/FileUploadProvider.swift
@@ -13,4 +13,34 @@ class FileUploadProvider {
     
     static let shared = { FileUploadProvider() }()
     
+    init() {
+        print("FileUploadProvider init")
+    }
+    
+    func requestFileUpload(parameters: Parameters, fileData: Data, fileName: String, mimeType: String,
+                           progressHandler: ((Progress) -> Void)?,
+                           successHandler: @escaping (AFDataResponse<Data?>) -> Void) {
+        guard let url = URL(string: "/api/upload", relativeTo: Constants.Base.domain.url) else {
+            return
+        }
+        
+//        guard let url = URL(string: "/transfer", relativeTo: Constants.Base.domain.url) else {
+//            return
+//        }
+        
+        AF.upload(multipartFormData: { multipart in
+            for (key, value) in parameters {
+                multipart.append("\(value)".data(using: String.Encoding.utf8)!, withName: key as String)
+            }
+            
+            multipart.append(fileData, withName: "files", fileName: fileName, mimeType: mimeType)
+            
+        }, to: url, method: .post)
+        .uploadProgress { progress in
+            progressHandler?(progress)
+            
+        }.response { response in
+            successHandler(response)
+        }
+    }
 }
--- FileUpload/FileUpload/ViewController.swift
+++ FileUpload/FileUpload/ViewController.swift
@@ -14,12 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var progressLabel: UILabel!
     @IBOutlet weak var uploadButton: UIButton!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func pressUploadButton(_ sender: Any) {
         performFileUpload()
     }
@@ -37,42 +37,22 @@ extension ViewController {
         
         do {
             let fileData = try? Data(contentsOf: filePath!)
-            requestFileUpload(parameters: parameters,
-                           fileData: fileData ?? Data(),
-                           fileName: fileName,
-                           mimeType: mimeType ?? "")
-        }
-    }
-    
-    func requestFileUpload(parameters: Parameters, fileData: Data, fileName: String, mimeType: String) {
-        guard let url = URL(string: "/api/upload", relativeTo: Constants.Base.domain.url) else {
-            return
-        }
-        
-//        guard let url = URL(string: "/transfer", relativeTo: Constants.Base.domain.url) else {
-//            return
-//        }
-        
-        AF.upload(multipartFormData: { multipart in
-            for (key, value) in parameters {
-                multipart.append("\(value)".data(using: String.Encoding.utf8)!, withName: key as String)
-            }
-            
-            multipart.append(fileData, withName: "files", fileName: fileName, mimeType: mimeType)
-            
-        }, to: url, method: .post)
-        .uploadProgress { progress in
-            self.progressView.setProgress(Float(progress.fractionCompleted), animated: true)
-            self.progressLabel.text = "\(progress.fractionCompleted*100)"
-            
-        }.response { response in
-            if let data = response.data {
-                print("Response Data: \(data)")
-                print("response : \(String(describing: String(data: data, encoding: .utf8)))")
-                
-            } else {
-                print("Something went wrong")
-            }
+            FileUploadProvider.shared.requestFileUpload(parameters: parameters,
+                              fileData: fileData ?? Data(),
+                              fileName: fileName,
+                              mimeType: mimeType ?? "",
+                              progressHandler: { progress in
+                                self.progressView.setProgress(Float(progress.fractionCompleted), animated: true)
+                                self.progressLabel.text = "\(progress.fractionCompleted*100)"
+                              }, successHandler: { response in
+                                if let data = response.data {
+                                    print("Response Data: \(data)")
+                                    print("response : \(String(describing: String(data: data, encoding: .utf8)))")
+                                    
+                                } else {
+                                    print("Something went wrong")
+                                }
+                              })
         }
     }
 }
