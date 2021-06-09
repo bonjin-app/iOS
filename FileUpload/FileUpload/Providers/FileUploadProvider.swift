588d78257bb009af40580b15e8a1e46df11a7b7e
bonjin-all <bonjin.app@gmail.com>
2021-06-09 16:31:16 +0900

    Update FileUploadProvider 상수 선언.

--- FileUpload/FileUpload/Providers/FileUploadProvider.swift
+++ FileUpload/FileUpload/Providers/FileUploadProvider.swift
@@ -13,6 +13,11 @@ class FileUploadProvider {
     
     static let shared = { FileUploadProvider() }()
     
+    static let API_UPLOAD = "/api/upload"
+//    static let API_UPLOAD = "/transfer"
+    
+    static let FILE_UPLOAD_KEY = "files"
+    
     init() {
         print("FileUploadProvider init")
     }
@@ -20,20 +25,16 @@ class FileUploadProvider {
     func requestFileUpload(parameters: Parameters, fileData: Data, fileName: String, mimeType: String,
                            progressHandler: ((Progress) -> Void)?,
                            successHandler: @escaping (AFDataResponse<Data?>) -> Void) {
-        guard let url = URL(string: "/api/upload", relativeTo: Constants.Base.domain.url) else {
+        guard let url = URL(string: FileUploadProvider.API_UPLOAD, relativeTo: Constants.Base.domain.url) else {
             return
         }
-        
-//        guard let url = URL(string: "/transfer", relativeTo: Constants.Base.domain.url) else {
-//            return
-//        }
         
         AF.upload(multipartFormData: { multipart in
             for (key, value) in parameters {
                 multipart.append("\(value)".data(using: String.Encoding.utf8)!, withName: key as String)
             }
             
-            multipart.append(fileData, withName: "files", fileName: fileName, mimeType: mimeType)
+            multipart.append(fileData, withName: FileUploadProvider.FILE_UPLOAD_KEY, fileName: fileName, mimeType: mimeType)
             
         }, to: url, method: .post)
         .uploadProgress { progress in
