dfa33032e5ccceed509c6f486f23738de202b173
bonjin-all <bonjin.app@gmail.com>
2021-06-10 10:38:08 +0900

    Update ViewController: imagePicker 코드 개선.

--- ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     let imagePicker = UIImagePickerController()
-    var captureImage: UIImage! = nil
-    var videoURL: URL! = nil
     var flagImageSave: Bool = false
     
     override func viewDidLoad() {
@@ -26,58 +24,25 @@ class ViewController: UIViewController {
 
     @IBAction func pressCameraButton(_ sender: Any) {
         print("pressCameraButton")
+        flagImageSave = true
         
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            imagePicker.sourceType = .camera
-            imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera) ?? []
-            imagePicker.allowsEditing = false
-            present(imagePicker, animated: true, completion: nil)
+            presentPhotoPicker(sourceType: .camera)
             
         } else {
             showAlert("Camera inaccessble", message: "Application cannot access the camera")
         }
     }
     
-    @IBAction func pressImageLoadButton(_ sender: Any) {
+    @IBAction func pressPhotoLibraryButton(_ sender: Any) {
         print("pressImageLoadButton")
+        flagImageSave = false
         
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            
-            imagePicker.sourceType = .photoLibrary
-            imagePicker.mediaTypes = [kUTTypeMovie as String]
-            imagePicker.allowsEditing = false
-            present(imagePicker, animated: true, completion: nil)
+            presentPhotoPicker(sourceType: .photoLibrary)
             
         } else {
             showAlert("Photo album inaccessable", message: "Application cannot access the photo album.")
-        }
-    }
-    
-    @IBAction func pressVideoButton(_ sender: Any) {
-        print("pressVideoButton")
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            imagePicker.sourceType = .camera
-            imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera) ?? []
-            imagePicker.allowsEditing = false
-            present(imagePicker, animated: true, completion: nil)
-            
-        } else {
-            showAlert("Camera inaccessble", message: "Application cannot access the camera")
-        }
-    }
-    
-    @IBAction func pressVideoLoadButton(_ sender: Any) {
-        print("pressVideoLoadButton")
-        
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            imagePicker.sourceType = .photoLibrary
-            imagePicker.mediaTypes = [kUTTypeMovie as String]
-            imagePicker.allowsEditing = false
-            present(imagePicker, animated: true, completion: nil)
-            
-        } else {
-            showAlert("Photo album inaccessable", message: "Application cannot access the photo album")
         }
     }
 }
@@ -89,22 +54,30 @@ class ViewController: UIViewController {
  
  imagePicker.mediaTypes = [kUTTypeImage as String]
  imagePicker.mediaTypes = [kUTTypeMovie as String]
+ or
+ imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera) ?? []
  */
 extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         print("imagePickerController didFinishPickingMediaWithInfo")
         
-        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! String
         
-        if mediaType.isEqual(to: kUTTypeImage as NSString as String) {
-            captureImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-            UIImageWriteToSavedPhotosAlbum(captureImage, self, nil, nil)
+        if mediaType.isEqual(kUTTypeImage as String) {
+            let captureImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
             imageView.image = captureImage
             
-        } else if mediaType.isEqual(to: kUTTypeMovie as NSString as String) {
-            videoURL = (info[UIImagePickerController.InfoKey.mediaURL] as! URL)
-            UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, self, nil, nil)
+            if flagImageSave {
+                UIImageWriteToSavedPhotosAlbum(captureImage, self, nil, nil)
+            }
+            
+        } else if mediaType.isEqual(kUTTypeMovie as String) {
+            let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as! URL
             imageView.image = videoSnapshot(path: videoURL.relativePath)
+            
+            if flagImageSave {
+                UISaveVideoAtPathToSavedPhotosAlbum(videoURL.relativePath, self, nil, nil)
+            }
         }
         self.dismiss(animated: true, completion: nil)
     }
@@ -116,6 +89,12 @@ extension ViewController: UINavigationControllerDelegate, UIImagePickerControlle
 }
 
 extension ViewController {
+    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        imagePicker.sourceType = sourceType
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera) ?? []
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     func showAlert(_ title: String, message: String) {
         let alertViewController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let alertAction = UIAlertAction(title: "확인", style: .default, handler: nil)
