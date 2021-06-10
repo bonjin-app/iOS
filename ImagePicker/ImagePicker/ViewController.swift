bb8edc74b45dce6e2da84e93e2bce3d7e0ddb1a3
bonjin-all <bonjin.app@gmail.com>
2021-06-10 09:49:38 +0900

    Update ViewController: Button Action 연결

--- ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressCameraButton(_ sender: Any) {
+        print("pressCameraButton")
+    }
+    
+    @IBAction func pressImageLoadButton(_ sender: Any) {
+        print("pressImageLoadButton")
+    }
+    
+    @IBAction func pressVideoButton(_ sender: Any) {
+        print("pressVideoButton")
+    }
+    
+    @IBAction func pressVideoLoadButton(_ sender: Any) {
+        print("pressVideoLoadButton")
+    }
 }
 
